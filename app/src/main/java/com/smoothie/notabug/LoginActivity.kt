454831355ac8457b04f2afa41e7422007455b3de
Smooth-E <dimbar2005@gmail.com>
2022-09-27 23:55:39 +0500

    Tweaked status and navigation bars' colors in themes

--- app/src/main/java/com/smoothie/notabug/LoginActivity.kt
+++ app/src/main/java/com/smoothie/notabug/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.smoothie.notabug
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_alt)
+        setContentView(R.layout.activity_login)
     }
 }
