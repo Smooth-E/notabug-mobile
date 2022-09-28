5aea3e5aa38df096469b7ab33495d287ce391795
Smooth-E <dimbar2005@gmail.com>
2022-09-28 18:59:57 +0500

    Switched to more simple bottom navigation bar

--- app/src/main/java/com/smoothie/notabug/anonymous/BrowsingActivity.kt
+++ app/src/main/java/com/smoothie/notabug/anonymous/BrowsingActivity.kt
@@ -2,13 +2,25 @@ package com.smoothie.notabug.anonymous
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.smoothie.notabug.R
 
 class BrowsingActivity : AppCompatActivity() {
 
+    private lateinit var bottomNavigationBar : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anonymous_main)
+
+        bottomNavigationBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.tab_code -> { true }
+                R.id.tab_people -> { true }
+                R.id.tab_settings -> { true }
+                else -> false
+            }
+        }
     }
 
 }
