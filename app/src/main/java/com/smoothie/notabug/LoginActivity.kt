23efedc5bbdd101785b91463f4b898413e09f03d
Smooth-E <dimbar2005@gmail.com>
2022-09-28 16:36:48 +0500

    Implemented snackbar if some fields are empty on login screen

--- app/src/main/java/com/smoothie/notabug/LoginActivity.kt
+++ app/src/main/java/com/smoothie/notabug/LoginActivity.kt
@@ -1,11 +1,44 @@
 package com.smoothie.notabug
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var rootViewGroup : ViewGroup
+
+    private lateinit var buttonLogin : Button;
+    private lateinit var buttonRegister : Button
+    private lateinit var buttonSkipLogin : Button
+
+    private lateinit var textInputUsername : TextInputLayout
+    private lateinit var textInputPassword: TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        rootViewGroup = findViewById(R.id.view_root)
+
+        buttonLogin = findViewById(R.id.button_login)
+        buttonRegister = findViewById(R.id.button_register)
+        buttonSkipLogin = findViewById(R.id.button_skip_login)
+
+        textInputUsername = findViewById(R.id.text_input_username)
+        textInputPassword = findViewById(R.id.text_input_password)
+
+        buttonLogin.setOnClickListener {
+            if (textInputUsername.editText!!.text.isEmpty() or textInputPassword.editText!!.text.isEmpty()) {
+                Snackbar.make(rootViewGroup, R.string.description_empty_fields, Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            // TODO: Implement login
+        }
     }
 }
