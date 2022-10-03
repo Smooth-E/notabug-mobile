1d32bd5a878275abc4e4493a69f1e1cb9c1a1303
Smooth-E <dimbar2005@gmail.com>
2022-10-03 08:27:54 +0500

    Fix RecyclerView scrolling automatically when adding items

--- app/src/main/java/com/smoothie/notabug/CodeRecyclerViewFragment.kt
+++ app/src/main/java/com/smoothie/notabug/CodeRecyclerViewFragment.kt
@@ -32,28 +32,27 @@ open class CodeRecyclerViewFragment : ScrollerRecyclerViewFragment<CodeRecyclerV
                 pageNumber++
                 val document = Jsoup.connect("${connectionUrl}?page=$pageNumber").get()
                 val repositories = document.body().getElementsByClass("ui repository list")[0]
+                for (element in repositories.getElementsByClass("item")) {
+                    val header = element.getElementsByClass("ui header")[0]
+                    val name = element.getElementsByClass("name")[0]
+                    val stats = header.getElementsByClass("ui right metas")[0].getElementsByTag("span")
+                    val possibleDescription = element.getElementsByClass("has-emoji")
+                    val holder = CodeRecyclerViewAdapter.DataHolder(
+                        url = name.attr("href"),
+                        icon = iconResource,
+                        name = name.text(),
+                        description = if (possibleDescription.size > 0) possibleDescription[0].text() else "",
+                        modificationDate = element.getElementsByClass("time")[0].text(),
+                        stars = stats[0].text().toInt(),
+                        forks = stats[1].text().toInt()
+                    )
+                    activity?.runOnUiThread { data.add(holder) }
+                }
                 activity?.runOnUiThread {
-                    for (element in repositories.getElementsByClass("item")) {
-                        val header = element.getElementsByClass("ui header")[0]
-                        val name = element.getElementsByClass("name")[0]
-                        val stats = header.getElementsByClass("ui right metas")[0].getElementsByTag("span")
-                        val possibleDescription = element.getElementsByClass("has-emoji")
-                        val holder = CodeRecyclerViewAdapter.DataHolder(
-                            url = name.attr("href"),
-                            icon = iconResource,
-                            name = name.text(),
-                            description = if (possibleDescription.size > 0) possibleDescription[0].text() else "",
-                            modificationDate = element.getElementsByClass("time")[0].text(),
-                            stars = stats[0].text().toInt(),
-                            forks = stats[1].text().toInt()
-                        )
-                        data.add(holder)
-                        // if (pageNumber > 1) recyclerView.adapter?.notifyItemInserted(data.size - 1)
-                    }
                     recyclerView.adapter?.notifyItemRangeInserted(pageNumber * 20 - 20, 20)
                     if (pageNumber <= 1) {
-                        // recyclerView.layoutManager?.scrollToPosition(0)
-                        // (recyclerView.adapter as CodeRecyclerViewAdapter).notifyItemRangeInserted(0, 20)
+                        // Scroll manually or else recycler scrolls to the bottom automatically
+                        recyclerView.layoutManager?.scrollToPosition(0)
                         swipeRefreshLayout.isRefreshing = false
                     }
                 }
