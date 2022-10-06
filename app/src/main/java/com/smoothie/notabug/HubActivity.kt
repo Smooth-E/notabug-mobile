c6faa248bab80ee4a7a1ef771939b7dc43290ebb
Smooth-E <dimbar2005@gmail.com>
2022-10-07 02:04:16 +0500

    Create LimitlessActivity and make HubActivity limitless with it

--- app/src/main/java/com/smoothie/notabug/HubActivity.kt
+++ app/src/main/java/com/smoothie/notabug/HubActivity.kt
@@ -1,12 +1,19 @@
 package com.smoothie.notabug
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnAttachStateChangeListener
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.doOnAttach
 import androidx.fragment.app.FragmentContainerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.elevation.SurfaceColors
 
-abstract class HubActivity(private val menuResource: Int, private val launchTabId: Int) : AppCompatActivity() {
+abstract class HubActivity(private val menuResource: Int, private val launchTabId: Int) : LimitlessActivity() {
 
     private lateinit var bottomNavigationBar : BottomNavigationView
     private lateinit var fragmentContainer : FragmentContainerView
@@ -25,12 +32,12 @@ abstract class HubActivity(private val menuResource: Int, private val launchTabI
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hub)
 
-        val surfaceColor = SurfaceColors.getColorForElevation(this, 8f)
-        window.navigationBarColor = surfaceColor
-
         bottomNavigationBar = findViewById(R.id.bottom_navigation_bar)
         fragmentContainer = findViewById(R.id.fragment_container_view)
 
+        fragmentContainer.doOnAttach { it.setPadding(it.paddingLeft, systemBarInsets.top + it.paddingTop, it.paddingRight, it.paddingBottom) }
+
+        bottomNavigationBar.doOnAttach { it.setPadding(it.paddingLeft, it.paddingTop, it.paddingRight, systemBarInsets.bottom + it.paddingBottom) }
         bottomNavigationBar.inflateMenu(menuResource)
         bottomNavigationBar.setOnItemSelectedListener { item ->
             performTransaction(item.itemId)
---
+++ app/src/main/java/com/smoothie/notabug/LimitlessActivity.kt
@@ -0,0 +1,34 @@
+package com.smoothie.notabug
+
+import android.graphics.Insets
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.doOnAttach
+
+open class LimitlessActivity : AppCompatActivity() {
+
+    protected lateinit var insets: WindowInsets
+    protected lateinit var systemBarInsets : Insets
+
+    private fun manageLimits() {
+        window.decorView.doOnAttach {
+            insets = it.rootWindowInsets
+            systemBarInsets = insets.getInsets(WindowInsets.Type.systemBars())
+        }
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        manageLimits()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        manageLimits()
+    }
+
+}
